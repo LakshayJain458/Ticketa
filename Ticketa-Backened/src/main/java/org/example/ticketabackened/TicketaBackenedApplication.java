b130743892832c245b411c36787e8d0921ec21be
LakshayJain458 <aalvpbjain93@gmail.com>
2025-07-04 20:21:58 +0530

    project created

---
+++ Ticketa-Backened/src/main/java/org/example/ticketabackened/TicketaBackenedApplication.java
@@ -0,0 +1,13 @@
+package org.example.ticketabackened;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketaBackenedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketaBackenedApplication.class, args);
+    }
+
+}
